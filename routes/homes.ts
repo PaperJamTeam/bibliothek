6184e16f8301e9cea2cab6f0c23cdf5571077e09
Георги Харизанов <gharizanov92@gmail.com>
2015-07-20 23:12:29 +0300

    added removing of homes

--- routes/homes.ts
+++ routes/homes.ts
@@ -1,94 +1,93 @@
 /// <reference path="../typings/tsd.d.ts" />
-
-import express = require('express');
-import mongoose = require('mongoose');
-
-import Home = require('../models/Home');
-
+var express = require('express');
+var mongoose = require('mongoose');
+var Home = require('../models/Home');
 var router = express.Router();
-
 /* GET homes page. */
-router.get('/', (req, res) => {
+router.get('/', function (req, res) {
 	res.render('homes');
 });
-
-router.get('/data', (req, res) => {
+router.get('/data', function (req, res) {
 	var page = req.query['page'];
 	var max = req.query['max'];
 	var sidx = req.query['sidx'];
 	var order = req.query['sord'];
 	var sort = {};
-
-	if(sidx && sidx !== ''){
+	if (sidx && sidx !== '') {
 		sort[sidx] = order;
 	}
-
-	Home.count((err, total) => {
-		if(!err){
-			Home.find({})
-				.sort(sort)
-				.limit(max)
-				.skip((page - 1) * max)
-				.exec((err, homes) =>{
-					if(!err){
-						var result = {
-							rows:homes,
-							page:page,
-							max:max,
-							total: total
-						};
-						res.json(result);
-					} else {
-						res.status(500).end();
-					}
-				});
-		} else {
+	Home.count(function (err, total) {
+		if (!err) {
+			Home.find({}).sort(sort).limit(max).skip((page - 1) * max).exec(function (err, homes) {
+				if (!err) {
+					var result = {
+						rows: homes,
+						page: page,
+						max: max,
+						total: total
+					};
+					res.json(result);
+				}
+				else {
+					res.status(500).end();
+				}
+			});
+		}
+		else {
 			res.status(500).end();
 		}
 	});
 });
-
-router.post('/data', (req, res) => {
-<<<<<<< HEAD
+router.post('/data', function (req, res) {
 	var data = req.body;
-	console.log(data);
-
 	var request_type = data['oper'];
+
 	delete data['oper'];
 	delete data['id'];
 
-	if(request_type === 'add'){
+	if (request_type === 'add') {
 		var id = mongoose.Types.ObjectId();
 		var home = new Home(data);
-
-=======
-	var request_type = req.body['oper'];
-	var id = mongoose.Types.ObjectId();
-
-	if(request_type === 'add'){
-		var home = new Home(req.body);
->>>>>>> origin/master
 		home["_id"] = id;
-		home.save( (err, res) => {
-			console.log(res);
-			if(!err){
+		home.save(function (err, result) {
+			console.log(result);
+			if (!err) {
 				res.status(201).end();
-			} else {
+			}
+			else {
 				res.status(500).end();
 			}
-		})
-	} else {
-		if(request_type === 'edit'){
+		});
+	}
+	else {
+		var _id = data['_id'];
+		delete data['_id'];
 
-<<<<<<< HEAD
-			var _id = data['_id'];
-			delete data['_id'];
-=======
->>>>>>> origin/master
+		if (request_type === 'edit') {
+
+			Home.update({_id:_id}, data, {}, function(err, result) {
+				console.log(result);
+				if (!err) {
+					res.status(200).end();
+				}
+				else {
+					res.status(500).end();
+				}
+			});
+		} else {
+			if(request_type === 'del'){
+				console.log(_id);
+				Home.remove({_id : _id}, function(err, result){
+					if (!err) {
+						res.status(200).end();
+					}
+					else {
+						res.status(500).end();
+					}
+				});
+			}
 		}
 	}
-
-
 });
-
-export = router;
+module.exports = router;
+//# sourceMappingURL=homes.js.map
