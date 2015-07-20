d31412e4334384d8e738f6cb0569f08d36dca7c8
Георги Харизанов <gharizanov92@gmail.com>
2015-07-21 00:49:17 +0300

    searching for homes page initial

--- routes/homes.ts
+++ routes/homes.ts
@@ -4,6 +4,7 @@ import mongoose = require('mongoose');
 import Home = require('../models/Home');
 
 var router = express.Router();
+
 /* GET homes page. */
 router.get('/', (req, res) => {
 	res.render('homes');
@@ -14,13 +15,81 @@ router.get('/data', (req, res) => {
 	var max = req.query['max'];
 	var sidx = req.query['sidx'];
 	var order = req.query['sord'];
+	var filters = req.query['filters'];
 	var sort = {};
+
+
+	var grouped_filters = [];
+	var filter_data = {};
+
+	if(filters) {
+		filters = JSON.parse(filters);
+
+		filters['rules'].forEach((filter) => {
+			var filter_entry = {};
+			switch (filter['op']) {
+				case "eq": { //equal
+					filter_entry[filter['field']] = filter['data'];
+					break;
+				}
+				case "ne": { //not equal
+					filter_entry[filter['field']] = {$ne: filter['data']};
+					break;
+				}
+				case "bw": { //begins with
+					filter_entry[filter['field']] = new RegExp("^" + filter['data'] + ".*");
+					break;
+				}
+				case "bn": { //does not begin with
+					filter_entry[filter['field']] = {$not: new RegExp("^" + filter['data'] + ".*")};
+					break;
+				}
+				case "ew": { //ends with
+					filter_entry[filter['field']] = new RegExp(".*" + filter['data']);
+					break;
+				}
+				case "en": { //does not end with
+					filter_entry[filter['field']] = {$not: new RegExp(".*" + filter['data'])};
+					break;
+				}
+				case "cn": { //contains
+					filter_entry[filter['field']] = new RegExp(".*" + filter['data'] + ".*");
+					break;
+				}
+				case "nc": { //does not contain
+					filter_entry[filter['field']] = {$not: new RegExp(".*" + filter['data']  + ".*")};
+					break;
+				}
+				case "nu": { //not equal
+					filter_entry[filter['field']] = new RegExp(" *");
+					break;
+				}
+				case "nn": { //not equal
+					filter_entry[filter['field']] = {$not: new RegExp(" *")};
+					break;
+				}
+			}
+			grouped_filters.push(filter_entry);
+		});
+
+		console.log(grouped_filters);
+
+		if(filters['groupOp'] === "AND"){
+			filter_data = {$and: grouped_filters}
+		} else {
+			filter_data = {$or: grouped_filters}
+		}
+
+		console.log(filter_data);
+	}
+
 	if (sidx && sidx !== '') {
 		sort[sidx] = order;
 	}
+
 	Home.count((err, total) => {
 		if (!err) {
-			Home.find({}).sort(sort).limit(max).skip((page - 1) * max).exec((err, homes) => {
+			Home.find(filter_data).sort(sort).limit(max).skip((page - 1) * max).exec((err, homes) => {
 				if (!err) {
 					var result = {
 						rows: homes,
@@ -29,13 +98,11 @@ router.get('/data', (req, res) => {
 						total: total
 					};
 					res.json(result);
-				}
-				else {
-					res.status(500).end();
+				} else {
+					res.status(500).send(err);
 				}
 			});
-		}
-		else {
+		} else {
 			res.status(500).end();
 		}
 	});
@@ -49,9 +116,11 @@ router.post('/data', (req, res) => {
 	delete data['id'];
 
 	if (request_type === 'add') {
+
 		var id = new mongoose.Types.ObjectId();
 		var home = new Home(data);
 		home["_id"] = id;
+
 		home.save((err, result) => {
 			console.log(result);
 			if (!err) {
@@ -61,8 +130,8 @@ router.post('/data', (req, res) => {
 				res.status(500).end();
 			}
 		});
-	}
-	else {
+
+	} else {
 		var _id = data['_id'];
 		delete data['_id'];
 
